7eabde6cf684af99a5679f78a426bf9306546c7f
John Souza <souzajohnlee@gmail.com>
2020-02-03 23:51:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,9 @@ def load_library(library)
   # code goes here
   emoticon_library = YAML.load_file(library)
   hash = {:get_meaning => {}, :get_emoticon => {}}
+  emoticon_library.each do |meaning|
+    hash[:get_meaning][meaning] = meaning[0]
+    hash[:get_emoticon][meaning] = meaning[1]
 end
 
 def get_japanese_emoticon
